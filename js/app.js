964a7cb1306334127836681f31b9d4a9dfdd92b6
haxiomic <haxiomic@gmail.com>
2015-06-23 16:54:54 +0100

    Fake shadows and fly through

--- js/app.js
+++ js/app.js
@@ -16,25 +16,26 @@ var app = (function(){
 	var renderCube;
 	var screenPlane;
 
-	var shaders = {
-		screenProjectionVS: null,
-		faceCoordinatesFS: null,
-		raytraceCloudsFS: null
-	};
-
+	//rendering
+	var shaders;//automatically populated from html
+	//materials
 	var backfacePassMaterial;
 	var raytracePassMaterial;
-
+	//render targets
 	var backfaceRenderTarget;
 	var screenbufferRenderTarget;
+	//misc
+	var cameraNormal;
 
-	var initScreenWidth;
-	var initScreenHeight;
+	//settings
+	var cubeRenderScaleFactor = parseFloat(URLParams.get("s", 0.3));
+	var sunInclination = parseFloat(URLParams.get("inc", 0.35));
+	var sunAzimuth = parseFloat(URLParams.get("az", 0.35));
+
+	var initTime;
+	var time;
 
 	function init(){
-		initScreenWidth = getScreenWidth();
-		initScreenHeight = getScreenHeight();
-
 		//load external resources
 		var noiseTexture = THREE.ImageUtils.loadTexture("images/noise.png");
 		noiseTexture.magFilter = THREE.LinearFilter;
@@ -46,13 +47,16 @@ var app = (function(){
 
 		//create objects
 		renderer = new THREE.WebGLRenderer();
-		cubeCamera = new THREE.PerspectiveCamera(50, getDrawAspect(), 0.001, 2000000);
+		cubeCamera = new THREE.PerspectiveCamera(80, getDrawAspect(), 0.001, 2000000);
 		screenPlaneCamera = new THREE.OrthographicCamera();
 
 		cubeScene = new THREE.Scene();
 		screenPlaneScene = new THREE.Scene();
 		skyScene = new THREE.Scene();
 		controls = new THREE.OrbitControls(cubeCamera);
+		cameraNormal = new THREE.Vector3(0,0,-1);
+		var sunPosition = computeSunPosition(sunInclination, sunAzimuth);
+
 
 		//set properties
 		//renderer
@@ -62,7 +66,7 @@ var app = (function(){
 		renderer.setClearColor(0x000000, 0);
 		renderer.setPixelRatio(window.devicePixelRatio);
 		//cameras
-		cubeCamera.position.z = 4.0;
+		cubeCamera.position.z = 2.0;
 		screenPlaneCamera.position.z = 1;
 		//controls
 		controls.damping = 0.2;
@@ -70,11 +74,7 @@ var app = (function(){
 		controls.addEventListener('change', render);
 
 		//get shaders from DOM
-		for(var key in shaders){
-			if(shaders.hasOwnProperty(key)){
-				shaders[key] = document.getElementById(key).textContent;
-			}
-		}
+		shaders = ShaderTools.getDOMShaders();
 
 		//render targets
 		backfaceRenderTarget = new THREE.WebGLRenderTarget(getDrawWidth(), getDrawHeight(), {
@@ -101,6 +101,12 @@ var app = (function(){
 		backfacePassMaterial = new THREE.ShaderMaterial({
 			vertexShader: shaders.screenProjectionVS,
 			fragmentShader: shaders.faceCoordinatesFS,
+			uniform: {
+				cameraNormal: {
+					type: "v3",
+					value: cameraNormal
+				}
+			},
 			side: THREE.BackSide
 		});
 
@@ -108,6 +114,10 @@ var app = (function(){
 			vertexShader: shaders.screenProjectionVS,
 			fragmentShader: shaders.raytraceCloudsFS,
 			uniforms: {
+				cameraNormal: {
+					type: "v3",
+					value: cameraNormal
+				},
 				backfaceInvResolution: {
 					type: "v2",
 					value: new THREE.Vector2(1/getDrawWidth(), 1/getDrawHeight())
@@ -119,35 +129,38 @@ var app = (function(){
 				noiseTexture: {
 					type: "t",
 					value: noiseTexture
+				},
+				sunPosition: {
+					type: "v3",
+					value: sunPosition
+				},
+				time: {
+					type: "f",
+					value: 0
 				}
 			},
-			transparent: true,
-			blending: THREE.AdditiveBlending
+			transparent: false,
+			// blending: THREE.AdditiveBlending,
+			// blending: THREE.CustomBlending,
+			// blendEquation: THREE.AddEquation,
+			// blendSrc: THREE.OneFactor,
+			// blendDst: THREE.OneFactor
 		});
 
 		//scene
+
+		//cube scene
 		renderCube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), null);
 		cubeScene.add(renderCube);
 
-		//add sky
+		//sky scene
+		//create sky
 		sky = new THREE.Sky();
 		sky.uniforms.turbidity.value = 10;
 		sky.uniforms.reileigh.value = 2;
 		sky.uniforms.mieCoefficient.value = 0.005;
 		sky.uniforms.mieDirectionalG.value = 0.8;
 		sky.uniforms.luminance.value = 1;
-
-		//compute sun position from inclination and az
-		var inc = 0.45;
-		var az = 0.25;
-		var theta = Math.PI * (inc - 0.5);
-		var phi = 2 * Math.PI * (az - 0.5);
-		var distance = 1;
-
-		var sunPosition = new THREE.Vector3();
-		sunPosition.x = distance * Math.cos(phi);
-		sunPosition.y = distance * Math.sin(phi) * Math.sin(theta);
-		sunPosition.z = distance * Math.sin(phi) * Math.cos(theta);
 
 		sky.uniforms.sunPosition.value = sunPosition;
 		skyScene.add(sky.mesh);
@@ -175,11 +188,17 @@ var app = (function(){
 						value: screenbufferRenderTarget
 					}
 				},
-				transparent: true
+				transparent: true,
+				blending: THREE.CustomBlending,
+				blendEquation: THREE.AddEquation,
+				blendSrc: THREE.SrcAlphaFactor,
+				blendDst: THREE.OneMinusSrcAlphaFactor
 			})
 		);
 		screenPlaneScene.add(screenPlane);
 
+
+		initTime = Date.now();
 		mainLoop();
 
 		//register event listeners
@@ -190,6 +209,14 @@ var app = (function(){
 	}
 
 	function mainLoop(){
+		//update time
+		time = Date.now() - initTime;
+		raytracePassMaterial.uniforms.time.value = time / 1000;
+
+		//update camera normal
+		cameraNormal.set(0,0,-1);
+		cameraNormal.applyQuaternion(cubeCamera.quaternion);
+
 		controls.update();
 		// controls.rotateLeft(0.01); //spin
 		render();
@@ -211,14 +238,16 @@ var app = (function(){
 		//screen pass
 		renderer.setViewport (0, 0, getScreenWidth(), getScreenHeight());
 
-		// renderer.state.setBlending(THREE.SubtractiveBlending);
+		//@! needs proper blending for screenPlaneScene onto skyScene
+		// renderer.state.setBlending(THREE.CustomBlending, THREE.SubtractEquation, THREE.OneFactor, THREE.OneFactor);
+
 		renderer.autoClearColor = false;
 		renderer.render(skyScene, cubeCamera, null, false);
 		renderer.render(screenPlaneScene, screenPlaneCamera, null, false);
 		renderer.autoClearColor = true;
+
 		// renderer.state.setBlending(THREE.NoBlending);
 	}
-
 
 	//Events
 	function onWindowResize(){
@@ -237,12 +266,25 @@ var app = (function(){
 	}
 
 	//
+	function computeSunPosition(inclination, azimuth){
+		//compute sun position from inclination and az
+		var theta = Math.PI * (inclination - 0.5);
+		var phi = 2 * Math.PI * (azimuth - 0.5);
+		var distance = 1;
+
+		var sunPosition = new THREE.Vector3();
+		sunPosition.x = distance * Math.cos(phi);
+		sunPosition.y = distance * Math.sin(phi) * Math.sin(theta);
+		sunPosition.z = distance * Math.sin(phi) * Math.cos(theta);
+		return sunPosition;
+	}
+
 	function getScreenWidth(){return window.innerWidth;}
 	function getScreenHeight(){return window.innerHeight;}
 	function getScreenAspect(){return getScreenWidth() / getScreenHeight();}
 
-	function getDrawWidth(){return window.innerWidth * .25;}
-	function getDrawHeight(){return window.innerHeight * .25;}
+	function getDrawWidth(){return window.innerWidth * cubeRenderScaleFactor;}
+	function getDrawHeight(){return window.innerHeight * cubeRenderScaleFactor;}
 	function getDrawAspect(){return getDrawWidth() / getDrawHeight();}
 
 	return {
